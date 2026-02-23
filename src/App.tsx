1312a340cbff3ba4abee1a8d7a4541ed6042deec
Patrick Lafferty <p.lafferty@quantumaviation.co.uk>
2026-02-23 11:50:13 +0000

    base react/vite app generated, added tailwind, setup app architecture and cleaned up vulnerabilities

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white text-3xl">
+      Tailwind Working
+    </div>
+  )
+}
